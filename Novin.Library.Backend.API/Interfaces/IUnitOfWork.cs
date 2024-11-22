389fe8eb52f73bb954c035c85bbdca6af531dbbf
Shayan Pakrad <shaayanpakrad@gmail.com>
2024-11-22 22:29:58 +0330

    add IUnitOfWork interface

---
+++ Novin.Library.Backend.API/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Novin.Library.Backend.API.Entities.Base;
+
+namespace Novin.Library.Backend.API.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IRepository<TEntity> GetRepository<TEntity>() where TEntity : Thing;
+        DbSet<TEntity> GetDbSet<TEntity>() where TEntity : class;
+    }
+}
