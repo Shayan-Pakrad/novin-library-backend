b4f64c050c421ded4615b54c01eba6b6c1570b4f
Shayan Pakrad <shaayanpakrad@gmail.com>
2024-12-05 12:32:09 +0330

    modify program.cs to use controllers

--- Novin.Library.Backend.API/Program.cs
+++ Novin.Library.Backend.API/Program.cs
@@ -28,7 +28,7 @@ builder.Services.AddScoped<IRepository<Borrow>, BorrowRepository>();
 builder.Services.AddScoped<BookService, BookService>();
 builder.Services.AddScoped<SubscriberService, SubscriberService>();
 builder.Services.AddScoped<BorrowService, BorrowService>();
-
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -46,67 +46,69 @@ if (app.Environment.IsDevelopment())
 
 // Book CRUD
 
-app.MapGet("/books/list", (BookService bookService) =>
-{
-    return Results.Ok(bookService.List());
-});
-app.MapPost("/books/add", (BookService bookService, BookAddOrUpdateDto book) =>
-{
-    bookService.Add(book);
-});
-app.MapPut("/books/update/{guid}", (BookService bookService, string guid, BookAddOrUpdateDto book) =>
-{
-    bookService.Update(guid, book);
-});
-app.MapDelete("/books/delete/{guid}", (BookService bookService, string guid) =>
-{
-    bookService.Remove(guid);
-});
+// app.MapGet("/books/list", (BookService bookService) =>
+// {
+//     return Results.Ok(bookService.List());
+// });
+// app.MapPost("/books/add", (BookService bookService, BookAddOrUpdateDto book) =>
+// {
+//     bookService.Add(book);
+// });
+// app.MapPut("/books/update/{guid}", (BookService bookService, string guid, BookAddOrUpdateDto book) =>
+// {
+//     bookService.Update(guid, book);
+// });
+// app.MapDelete("/books/delete/{guid}", (BookService bookService, string guid) =>
+// {
+//     bookService.Remove(guid);
+// });
 
 
 
 
 // Subscriber CRUD
 
-app.MapGet("/subs/list", (SubscriberService subscriberService) =>
-{
-    return Results.Ok(subscriberService.List());
-});
-app.MapPost("/subs/add", (SubscriberService subscriberService, SubscriberAddOrUpdateDto sub) =>
-{
-    subscriberService.Add(sub);
-});
-app.MapPut("/subs/update/{guid}", (SubscriberService subscriberService, string guid, SubscriberAddOrUpdateDto sub) =>
-{
-    subscriberService.Update(guid, sub);
-});
-app.MapDelete("/subs/delete/{guid}", (SubscriberService subscriberService, string guid) =>
-{
-    subscriberService.Remove(guid);
-});
+// app.MapGet("/subs/list", (SubscriberService subscriberService) =>
+// {
+//     return Results.Ok(subscriberService.List());
+// });
+// app.MapPost("/subs/add", (SubscriberService subscriberService, SubscriberAddOrUpdateDto sub) =>
+// {
+//     subscriberService.Add(sub);
+// });
+// app.MapPut("/subs/update/{guid}", (SubscriberService subscriberService, string guid, SubscriberAddOrUpdateDto sub) =>
+// {
+//     subscriberService.Update(guid, sub);
+// });
+// app.MapDelete("/subs/delete/{guid}", (SubscriberService subscriberService, string guid) =>
+// {
+//     subscriberService.Remove(guid);
+// });
 
 
 
 
 
-// Borrow CRUD
+// // Borrow CRUD
 
-app.MapGet("/borrows/list", (BorrowService borrowService) =>
-{
-    return Results.Ok(borrowService.List());
-});
-app.MapPost("/borrows/add", (BorrowService borrowService, BorrowAddOrUpdateDto borrow) =>
-{
-    borrowService.Add(borrow);
-});
-app.MapPut("/borrows/update/{guid}", (BorrowService borrowService, string guid, BorrowAddOrUpdateDto borrow) =>
-{
-    borrowService.Update(guid, borrow);
-});
-app.MapDelete("/borrows/delete/{guid}", (BorrowService borrowService, string guid) =>
-{
-    borrowService.Remove(guid);
-});
+// app.MapGet("/borrows/list", (BorrowService borrowService) =>
+// {
+//     return Results.Ok(borrowService.List());
+// });
+// app.MapPost("/borrows/add", (BorrowService borrowService, BorrowAddOrUpdateDto borrow) =>
+// {
+//     borrowService.Add(borrow);
+// });
+// app.MapPut("/borrows/update/{guid}", (BorrowService borrowService, string guid, BorrowAddOrUpdateDto borrow) =>
+// {
+//     borrowService.Update(guid, borrow);
+// });
+// app.MapDelete("/borrows/delete/{guid}", (BorrowService borrowService, string guid) =>
+// {
+//     borrowService.Remove(guid);
+// });
 
+
+app.MapControllers();
 
 app.Run();
